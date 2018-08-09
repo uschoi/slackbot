8f9350b4d98bee4f23d290f3433ac2c58665050b
T Y <uschoi77@gmail.com>
2018-08-09 11:48:46 +0900

    make 3 module

--- main.py
+++ main.py
@@ -2,17 +2,13 @@ from rtmbot import RtmBot
 from rtmbot.core import Plugin
 
 import secret
-
+import chatlogic
 
 class HelloPlugin(Plugin):
     def process_message(self, data):
-        if "유신" in data["text"]:
-            self.outputs.append([data["channel"], "불렀어?"])
-        elif "주사위" == data["text"]:
-            die = str(random.randint(1, 6))
-            self.outputs.append([data["channel"], die])
-        else:
-            pass
+        reply = answer(data["text"])
+        if reply is not none:
+            self.outputs.append([data["channel"], reply])
 
 
 config = {
